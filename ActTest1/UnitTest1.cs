bd05ef51075aeefa320efdd532dfe485766544e5
Vinayak-AugmentoLabs <75657904+Vinayak-AugmentoLabs@users.noreply.github.com>
2020-12-08 18:31:11 +0530

    Add files via upload

--- ActTest1/UnitTest1.cs
+++ ActTest1/UnitTest1.cs
@@ -14,13 +14,47 @@ namespace ActTest1
             double p = 100;
             double r = 1;
             double expec1 = 500;
-            double expec2 = 5000;
+          
 
             //act
            double act1= SImple_Intrest.CalculateSI.CalCulate(p, y, r);
-           // double act2 = SImple_Intrest.CalculateSI.CalCulate(p, y, r);
+          
             Assert.AreEqual(expec1, act1);
-            //Assert.AreEqual(expec2, act2);
+           
+
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //expected
+            int y = 5;
+            double p = 1000;
+            double r = 1;
+            double expec1 = 5000;
+            
+            //act
+
+            double act1 = SImple_Intrest.CalculateSI.CalCulate(p, y, r);
+           
+            Assert.AreEqual(expec1, act1);
+          
+
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //expected
+            int y = 5;
+            double p = 100;
+            double r = 1;
+            double expec1 = 600;
+        
+
+            //act
+            double act1 = SImple_Intrest.CalculateSI.CalCulate(p, y, r);
+            
+            Assert.AreEqual(expec1, act1);
+           
 
         }
     }
